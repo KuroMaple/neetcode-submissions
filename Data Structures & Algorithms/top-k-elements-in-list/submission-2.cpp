118aee170daba8ee1e3f75c8e19e7bed40ac5ee5
Hassan Hashmi <46617735+KuroMaple@users.noreply.github.com>
2026-06-16 13:19:39 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // hash map to count freqs
+        unordered_map<int, int> freqMap;
+        for(int n: nums){
+            freqMap[n]++;
+        }
+        
+        vector<vector<int>> buckets(nums.size() + 1);
+        buckets.reserve(nums.size() + 1);
+        for(auto [val, freq] : freqMap){
+            buckets[freq].push_back(val);
+        }
+
+        vector<int> result;
+        result.reserve(k);
+        int bucketPointer = nums.size();
+        while(k != 0) {
+            if(buckets[bucketPointer].empty()){
+                bucketPointer--;
+                continue;
+            }
+
+
+            k -= buckets[bucketPointer].size();
+            // Move semantics
+            result.insert(result.end(), 
+            std::make_move_iterator(buckets[bucketPointer].begin()), 
+            std::make_move_iterator(buckets[bucketPointer].end()));
+            bucketPointer--;
+        }
+
+        return result;
+        
+    }
+};
