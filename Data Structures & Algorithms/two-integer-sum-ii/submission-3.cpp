1bd99f8650e3624bebc657a6c33520d7fb69acee
Hassan Hashmi <46617735+KuroMaple@users.noreply.github.com>
2026-06-20 15:24:02 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int leftPointer = 0;
+        int rightPointer = numbers.size() - 1;
+
+        while(leftPointer < rightPointer)
+        {
+            auto currentSum = numbers[leftPointer] + numbers[rightPointer];
+            if(currentSum < target)
+            {
+                ++leftPointer;
+            } 
+            else if(currentSum > target)
+            {
+                --rightPointer;
+            }
+            else
+            {
+                return {leftPointer + 1, rightPointer + 1};
+            }
+        }
+
+        return {};
+    }
+};
