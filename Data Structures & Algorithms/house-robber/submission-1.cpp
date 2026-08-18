2278d4da88f3ee7378a5bf622d1fc97a6e7e5e4c
Hassan Hashmi <46617735+KuroMaple@users.noreply.github.com>
2026-08-18 10:42:29 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // i = max(i + i - 2, i - 1)
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        std::vector<int> dp(n, -1);
+
+        dp[0] = nums[0];
+        dp[1] = std::max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; ++i) {
+            dp[i] = std::max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return std::max(dp[n - 1], dp[n - 2]);
+    }
+};
